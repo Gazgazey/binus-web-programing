075254a49754e1a14328ffb2cebd68340b120dee
Gazgazey <aquadragon360@gmail.com>
2025-02-20 16:26:48 +0700

    Create PyramidAccendingDecending.js

---
+++ JavaScript/Day4/PyramidAccendingDecending.js
@@ -0,0 +1,15 @@
+for (let a = 7; a > 0; a--) {
+    let right = "";
+    for (let b = 0; b < a; b++) {
+        right += a;
+    }
+    console.log(right);
+}
+
+for (let a = 2; a < 8; a++) {
+    let right = "";
+    for (let b = 0; b < a; b++) {
+        right += a;
+    }
+    console.log(right);
+}
